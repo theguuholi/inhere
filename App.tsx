17fb8f109614660b1a8823c3a9d4e3797dd6282c
Gus <g.92oliveira@gmail.com>
2024-12-18 09:04:13 -0300

    start

--- App.tsx
+++ App.tsx
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.eventName}>Event Name</Text>
+      <Text style={styles.eventDate}>Friday, 18 December 2024.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 24
   },
+  eventName: {
+    color: "#fdfcfe",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  }
+
 });
