13a75b7c91f7609f70271b1d4cd90077ad0492ff
Gus <g.92oliveira@gmail.com>
2024-12-18 09:10:26 -0300

    react 18

--- App.tsx
+++ App.tsx
@@ -1,31 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Event Name</Text>
-      <Text style={styles.eventDate}>Friday, 18 December 2024.</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24
-  },
-  eventName: {
-    color: "#fdfcfe",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 48
-  },
-  eventDate: {
-    color: "#6b6b6b",
-    fontSize: 16,
-  }
-
-});
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Event Name</Text>
+      <Text style={styles.eventDate}>Friday, 18 December 2024.</Text>
+    </View>
+  );
+}
